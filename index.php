cf1e1dda4fa61744bd1e7544795f161c2f3d6d9c
anjali07 <asen53184@gmail.com>
2021-06-01 12:08:38 +0530

    store value in variable

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel ="stylesheet" href="index.css">
+</head>
+<body>
+
+<?php
+$food = "burger";
+$food1 = "hot-dog";
+$food2 = "chinese";
+$food3 = "golgappe";
+
+  echo "<h1>You Can Order From The List!</h1>";
+   echo "<p>list here!</p>";
+    echo "<li>$food</li>";
+    echo "<li>$food1</li>"; 
+   echo "<li>$food2</li>"; 
+   echo "<li>$food3</li>"; 
+ ?>
+</body>
+</html>

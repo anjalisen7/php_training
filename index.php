146e9c619c098c0c69b99589cf3a6c375acd039d
anjali07 <asen53184@gmail.com>
2021-06-02 22:36:27 +0530

    login form(get method)

--- index.php
+++ index.php
@@ -5,18 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <link rel ="stylesheet" href="index.css">
 </head>
 <body>
-          <h1>LOGIN</h1>
-     <form action="index1.php" method="get">
-      <label>NAME</label>
-      <input type = "text" name="iname">
-      <label>PASSWORD</label>
-      <input type ="password" name="pass">
-      <button type ="submit">ok</button>
+    <div class="container">
+        <div class ="row">
+            <div class="col-sm-12">
+        <h1> LOGIN FORM</h1>
+        <img src ="img1506998160614.jpg" class="rounded-circle"> 
+    <form action="index1.php" method="get">
+      <label>E-MAIL</label><br>
+      <input type="email" id="text" placeholder="📧" name="fmail"><br><br>
+      <label>PASSWORD</label><br>
+      <input type="password" id="text1" placeholder=" 🔏" name="pass"><br><br>
+      <input type= "submit" id="btn" value="login">
 
-   <?php   
-       ?>
+   </form>
+</div>
+</div>
+</div>
+   
 </body>
 </html>
--- index1.php
+++ index1.php
@@ -5,11 +5,41 @@
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Document</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <link rel ="stylesheet" href="index.css">
  </head>
  <body>
- 
-    Welcome <?php echo $_GET["iname"]; ?><br>
-   Your password is: <?php echo $_GET["pass"]; ?>
+            
+    <?php 
+  
+    $mail = $_GET['fmail'];
+    $password=$_GET['pass'];
+    if(!empty($mail) && !empty($password)){
+        echo "<h1>welcome to our site</h1>"."<br>";
+        echo "<h4>your email id is $mail</h4>"."<br>";
+        echo "<h3>password is:'.$password<h3>";
+         
+    } else{
+         echo "<h1>oops!</h1>"."<br>";
+        echo "<h4> Error404: page not found</h4>"."<br>";
+        
+       
+    }
+  ?>
+ <style>
+    h1{
+        background-color: grey;
+        text-align:center;
+    }
+     h4{
+          text-align:center;
+          text-decoration:underline;
+     }
+      h3{
+          text-align:center;
+      }
+     </style>
 
  </body>
  </html>

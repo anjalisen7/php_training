a960afb05e2b05e544c1086e06de01847993be31
anjali07 <asen53184@gmail.com>
2021-06-16 21:53:15 +0530

    fpassthru(return left line)

--- fileread.php
+++ fileread.php
@@ -2,12 +2,10 @@
 
 $file =  fopen("myfile.txt","r");
 
-echo fgets($file);
-echo ftell($file);
 echo "<br>".fgets($file);
-echo ftell($file);
-  fseek($file,20);
-  echo "<br>".fgets($file);
+echo"<br>". ftell($file);
+echo "<br>".fpassthru($file);
+
 
  
 ?>

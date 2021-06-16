fbc5d555679c7d54c39b0186fad3c8df72ef4e14
anjali07 <asen53184@gmail.com>
2021-06-16 22:01:50 +0530

    feof(while)in ul

--- fileread.php
+++ fileread.php
@@ -1,11 +1,11 @@
 <?php
 
 $file =  fopen("myfile.txt","r");
-
+  echo "<ul>";
 while(!feof($file)){
     $line=fgets($file);
-    echo $line ."<br>";
+    echo  "<li>".$line ."</li>";
 }
-
+echo "<ul>";
  
 ?>

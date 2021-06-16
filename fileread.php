2f9a0296a926070d25367036cd435b521f76ba86
anjali07 <asen53184@gmail.com>
2021-06-16 21:50:06 +0530

    fseek(change cursor position)

--- fileread.php
+++ fileread.php
@@ -4,9 +4,10 @@ $file =  fopen("myfile.txt","r");
 
 echo fgets($file);
 echo ftell($file);
-echo fgets($file);
+echo "<br>".fgets($file);
 echo ftell($file);
- 
+  fseek($file,20);
+  echo "<br>".fgets($file);
 
  
 ?>

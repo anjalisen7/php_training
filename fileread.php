48f87db3a40e2203c7eeea81673f990dce1aa82c
anjali07 <asen53184@gmail.com>
2021-06-16 22:05:54 +0530

    fgetc(return single character)

--- fileread.php
+++ fileread.php
@@ -1,11 +1,9 @@
 <?php
 
 $file =  fopen("myfile.txt","r");
-  echo "<ul>";
-while(!feof($file)){
-    $line=fgets($file);
-    echo  "<li>".$line ."</li>";
-}
-echo "<ul>";
+ 
+    echo  fgetc($file);
+
+
  
 ?>

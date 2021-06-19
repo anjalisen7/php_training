25c143798043030f527eb2a3e82af677a25e0dd6
anjali07 <asen53184@gmail.com>
2021-06-19 11:11:16 +0530

    filter_validate_url with flag(query_rewuired)

--- filter.php
+++ filter.php
@@ -2,7 +2,7 @@
  $url = "https://in.search.yahoo.com/search?fr=mcafee&type=E210IN885G91506&p=codeigniter+tutorial";
  
 
- if(filter_var($url,FILTER_VALIDATE_URL)){
+ if(filter_var($url,FILTER_VALIDATE_URL,FILTER_FLAG_QUERY_REQUIRED)===false){
      echo "  valid url";
  }else{
     echo " Unvalid url";

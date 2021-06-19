b0b1e078e0362ddbd4c7001e869101583d61a888
anjali07 <asen53184@gmail.com>
2021-06-19 17:22:22 +0530

    assignment_conne

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+     $servername ="localhost";
+   $username="root";
+   $DbPassword ="";
+   $dbname ="anjali";
+   
+   $conn = mysqli_connect($servername, $username,$DbPassword,$dbname);
+    // if($conn){
+    //    echo "connection succesfull";
+    // }else{
+    //    echo "connection lost";
+    // }
+?>

9df3a3bb83ec7f7b1ba4f71d5ad1c83ee87e95b4
anjali07 <asen53184@gmail.com>
2021-06-19 12:09:19 +0530

    filter_sanitize_number_float(with filter_flag_allow_thousand)

--- filter.php
+++ filter.php
@@ -1,8 +1,8 @@
 <?php
- $var = "$$345&.90";
+ $var = "1,50,000";
  
 
- $var = (filter_var($var,FILTER_SANITIZE_NUMBER_FLOAT ,FILTER_FLAG_ALLOW_FRACTION));
+ $var = (filter_var($var,FILTER_SANITIZE_NUMBER_FLOAT ,FILTER_FLAG_ALLOW_THOUSAND));
    if(filter_var($var,FILTER_VALIDATE_FLOAT)){
      echo "<br> $var is a valid FLOAT";
  }else{

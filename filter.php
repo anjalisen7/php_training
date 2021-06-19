3fefb8e2608ea5a038e737c174895688fcc75a30
anjali07 <asen53184@gmail.com>
2021-06-19 11:59:46 +0530

    filter_sanitize_number_int(remove spaces and special keyword)

--- filter.php
+++ filter.php
@@ -1,12 +1,12 @@
 <?php
- $var = "https://www.anjali      sen.net";
+ $var = "$$345&.90";
  
 
- $var = (filter_var($var,FILTER_SANITIZE_URL));
-   if(filter_var($var,FILTER_VALIDATE_URL)){
-     echo "<br> $var is a valid URL";
+ $var = (filter_var($var,FILTER_SANITIZE_NUMBER_INT));
+   if(filter_var($var,FILTER_VALIDATE_INT)){
+     echo "<br> $var is a valid INTEGER";
  }else{
-    echo " <br> $var is not a valid URL";
+    echo " <br> $var is not a valid INTEGER";
  }
 
 

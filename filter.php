0420721c47e52290d3b5e563d3bcbd75beaf4fde
anjali07 <asen53184@gmail.com>
2021-06-19 11:20:51 +0530

    filter_validate_boolean(return null)

--- filter.php
+++ filter.php
@@ -1,8 +1,8 @@
 <?php
- $var = "true";
+ $var = "43535";
  
 
- var_dump($var,FILTER_VALIDATE_BOOLEAN,FILTER_NULL_ON_FAILURE);
+ var_dump(filter_var($var,FILTER_VALIDATE_BOOLEAN,FILTER_NULL_ON_FAILURE));
    if(filter_var($var,FILTER_VALIDATE_BOOLEAN)){
      echo "<br> $var is boolean";
  }else{

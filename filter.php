a7593a416ad6ecd93ec14490c61994b840fb3afd
anjali07 <asen53184@gmail.com>
2021-06-19 16:10:42 +0530

    filter_var_array

--- filter.php
+++ filter.php
@@ -1,9 +1,22 @@
 <?php
- $var = "<h1>life is & beautiful.</h1>";
- 
+$arr = array(
+   "emp_name" => "anjali sen",
+   "emp_age" => "42",
+   "emp_email" => "anjali.aasima.sen@gmail.com",
+);
+$filters = array(
+       "emp_name" => FILTER_SANITIZE_STRING,        
+       "emp_age" => array(
+           "filter" => FILTER_VALIDATE_INT,
+           "option" => array(
+               "min-range" => 18,
+               "max-range" => 45
+           )
+           ),
+           "emp_email" => FILTER_VALIDATE_EMAIL,        
+);
 
- echo (filter_var($var,FILTER_SANITIZE_ENCODED));
-  
-
-
+echo  "<pre>";
+ print_r(filter_var_array($arr,$filters));
+  echo "</pre>";
 ?>

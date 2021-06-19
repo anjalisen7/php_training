16f016e5554a070d0ac915df909747c2ec83c373
anjali07 <asen53184@gmail.com>
2021-06-19 11:35:15 +0530

    filter_validate_mac

--- filter.php
+++ filter.php
@@ -1,12 +1,12 @@
 <?php
- $var = "raashi_rj@yahoo.in";
+ $var = "FA-F9-DD-B2-5E-0D";
  
 
- var_dump(filter_var($var,FILTER_VALIDATE_EMAIL));
-   if(filter_var($var,FILTER_VALIDATE_EMAIL)){
-     echo "<br> $var is a valid email";
+//  var_dump(filter_var($var,FILTER_VALIDATE_EMAIL));
+   if(filter_var($var,FILTER_VALIDATE_MAC)){
+     echo "<br> $var is a valid MAC";
  }else{
-    echo " <br> $var is not a valid email";
+    echo " <br> $var is not a valid MAC";
  }
 
 

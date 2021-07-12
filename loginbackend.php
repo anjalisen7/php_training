e8ea834574cf381a2a9ad3c3d6ca568c21ac7957
anjali07 <asen53184@gmail.com>
2021-07-12 21:55:39 +0530

    login_backend

--- loginbackend.php
+++ loginbackend.php
@@ -1,50 +1,43 @@
 <?php
-
- $servername=  "localhost";
- $username ="root";
- $password="";
- $dbname ="anjali";
- $email=$_POST['email'];
-$password=$_POST['password'];
- 
- $conn = mysqli_connect('localhost', 'root', '', 'anjali');
+   require_once("connection.php");
+     $email=$_POST['email'];
+     $password=$_POST['password'];
 
  if($_SERVER["REQUEST_METHOD"]=="POST"){
      if(empty($_POST['email'])){
-         echo "Email Required";
+        echo "EMAIL Required.";
      }else{
          if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-             echo "Invalid E-mail";
+             echo "Invalid Email.";
          }else{
              if(empty($_POST['password'])){
-                 echo "Password Required";
+                 echo "Password Required.";
              }else{
-                 $query = "select * from users";
-                 $result= mysqli_query($conn,$query);
+                 $query = "select * from users where email ='$email' and isActive='1'";
+                 $result = mysqli_query($conn,$query);
                  if($result->num_rows > 0){
-                   
                      if($row = $result->fetch_assoc()){
-                        //   print_r($row);
-                            session_start();
+                
+                        session_start();
                             // $user [] =$row;
                         $_SESSION['user']=$row;
                      }
-                   
-                    
+                     $_SESSION['status']="Loggin succesful";
+                     $_SESSION['status_code']="success";
+                    //  echo $_SESSION['user']['fname']."logged in succesfully";
                     ?>
                     <script>
                       window.location.href = "http://localhost/php_training/profilepage.php";
                     </script>
                   <?php
                  }else{
-                      echo "go to signup page";
-                    // header:('location:signup.php');
+                      
+                   echo "<a href='signup.php'><h6 style='color:black; margin-right:2px; font-size:20px;'>Go to Signup</h6></a>"."<a style= 'color:purple; font-size:15px;margin-left:2px;' href='enable.php'>Enable</a>";
+
                  }
              }
          }
      }
  }
-  
 
-//   echo "login succesfullly";
 ?>

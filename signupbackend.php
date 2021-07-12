3a93802062a317c0ed5192a6019431014becfafc
anjali07 <asen53184@gmail.com>
2021-07-12 21:50:14 +0530

    signup_ backend

--- signupbackend.php
+++ signupbackend.php
@@ -1,31 +1,23 @@
 <?php
 
  session_start();
- $servername ="localhost";
- $username="root";
- $password ="";
- $dbname ="anjali";
+    require_once("connection.php");
   $fname = $_POST['fname'];
-  $lname = $_POST['lname'];
   $age = $_POST['age'];
   $email = $_POST['email'];
   $address = $_POST['address'];
   $city = $_POST['city'];
   $field = $_POST['field'];
+  $date=date("y/m/d");
+  // $image_path =$_POST['file'];
   $form_password = md5($_POST['password']);
-
-
-   $conn =new mysqli ($servername ,$username ,$password ,$dbname);
     if($_SERVER["REQUEST_METHOD"]=="POST"){
-      if(empty($_POST['fname'])||(empty($_POST['lname']))||(empty($_POST['age']))||(empty($_POST['email']))||(empty($_POST['password']))||(empty($_POST['address']))||(empty($_POST['city']))||(empty($_POST['field']))){
+      if(empty($_POST['fname'])||(empty($_POST['age']))||(empty($_POST['email']))||(empty($_POST['password']))||(empty($_POST['address']))||(empty($_POST['city']))||(empty($_POST['field']))){
         echo "*ALL fields are mandatory!";
       }else{
         if(!preg_match("/^[a-zA-Z-' ]*$/",$_POST['fname'])){
           echo "*Invalid Name";
         }else{
-          if(!preg_match("/^[a-zA-Z-' ]*$/",$_POST['lname'])){
-            echo "*Invalid Surname";
-          }else{
              if($_POST['age']<18){
                echo "*Age must be over 18";
              }else{
@@ -37,12 +29,12 @@
                   if(mysqli_num_rows($ee)>0){
                     echo "User already exits";
                   }else{
-                   $signupinsert ="INSERT INTO `users` (`fname`, `lname`, `age`, `email`, `password`, `address`, `city`,`field`) VALUES ('$fname', '$lname', '$age', '$email', '$form_password','$address','$city','$field')";
+                    // $image_path="upload/day.jpg";
+                   $signupinsert ="INSERT INTO `users` (`fname`, `age`, `email`, `password`, `address`, `city`,`field`,`created_on`) VALUES ('$fname', '$age', '$email', '$form_password','$address','$city','$field','$date')";
                    if(mysqli_query($conn,$signupinsert)){
                     // $user = [$fname,$lname,$age,$email,$form_password];
                      $user = array(
                            "fname"=>$fname,
-                           "lname"=>$lname,
                            "age" =>$age,
                            "email"=>$email,
                            "address"=>$address,
@@ -64,6 +56,5 @@
           }
         }
       }
-    }
 ?>
 
